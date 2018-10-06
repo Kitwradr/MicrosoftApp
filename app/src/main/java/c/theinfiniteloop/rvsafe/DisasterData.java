9211a4cd5bc930b12afb55602dfe598ede84c87b
Suhas <suhas.2ab@gmail.com>
2018-10-06 18:42:54 +0530

    Adding data classes

---
+++ app/src/main/java/c/theinfiniteloop/rvsafe/DisasterData.java
@@ -0,0 +1,14 @@
+public class DisasterData {
+
+    public int disaster_id;
+
+    public String disaster_name;
+
+    public String getDisaster_type;
+
+    public String image_url;
+
+    public int wantToHelp_id;
+
+    
+}
---
+++ app/src/main/java/c/theinfiniteloop/rvsafe/WantToHelpGroupData.java
@@ -0,0 +1,4 @@
+package c.theinfiniteloop.rvsafe;
+
+public class WantToHelpGroupData {
+}
