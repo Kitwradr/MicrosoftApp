f144d99d8e570fd277d0d4742e4ab9303e793651
Suhas <suhas.2ab@gmail.com>
2018-10-09 14:06:07 +0530

    restored am_i_safe

--- app/src/main/java/c/theinfiniteloop/rvsafe/QueryData.java
+++ app/src/main/java/c/theinfiniteloop/rvsafe/QueryData.java
@@ -6,12 +6,27 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.*;
+
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.bson.Document;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+
+
 
 
 public class QueryData {
@@ -78,9 +93,86 @@ public class QueryData {
 
         }
 
+        try {
+            sendDownlaodimagerequest(data.image_url);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
         return data;
 
     }
+
+    static void sendDownlaodimagerequest(String url ) throws Exception
+    {
+        URL obj = new URL(url);
+
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type","image/jpeg");
+        con.setDoInput(true);
+        con.setDoOutput(true);
+
+        try {
+
+
+            //Path path = Paths.get("jpeg.jpg");
+            byte[] fileContents = new byte[500];
+            //System.out.println(fileContents.length);
+            //System.out.println("Sending image to server. ");
+
+            OutputStream out = con.getOutputStream();
+            DataOutputStream image = new DataOutputStream(out);
+
+            //image.writeInt(bytes.length);
+            image.write(fileContents, 0, fileContents.length);
+
+            FileOutputStream stream = new FileOutputStream("C:\\Users\\suhas\\Desktop\\MicrosoftApp\\app\\src\\main\\res\\drawable");
+            try {
+                stream.write(fileContents);
+            } finally {
+                stream.close();
+            }
+
+
+
+
+            image.close();
+            // close the output stream
+            out.close();
+
+        }catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+
+        // define object for the reply from the server
+        BufferedInputStream in = new BufferedInputStream(con.getInputStream());
+        //Get response from server
+        int responseCode = con.getResponseCode();
+        System.out.println("Response Code : " + responseCode);
+//        // read in the response from the server
+//        String inputLine;
+//        StringBuffer response = new StringBuffer();
+//        while ((inputLine = in.readLine()) != null) {
+//            response.append(inputLine);
+//            //System.out.println(inputLine);
+//        }
+//        System.out.println(response.length());
+//        // close the input stream
+//        in.close();
+        byte[] b = org.apache.commons.io.IOUtils.toByteArray(in);
+        System.out.println(b.length);
+
+
+    }
+
+
+
+}
+
 
     public static WantToHelpGroupData queryWanttohelpdata(int id){
         morphia.mapPackage("c.theinfiniteloop.rvsafe");
