14e15b3120f5a2d6e6e214d35eb03bd528175b83
Suhas <suhas.2ab@gmail.com>
2018-10-13 18:06:15 +0530

    syncing

--- app/src/main/java/c/theinfiniteloop/rvsafe/RescueGroupData.java
+++ app/src/main/java/c/theinfiniteloop/rvsafe/RescueGroupData.java
@@ -81,7 +81,7 @@ public class RescueGroupData {
     }
 
     public String getContact_no() {
-        return  contact_no;
+        return contact_no;
     }
 
     public void setContact_no(String contact_no) {
